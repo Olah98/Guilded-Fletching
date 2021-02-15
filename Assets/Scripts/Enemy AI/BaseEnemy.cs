2df650781d421bc228d8a82d189120eba5d41802
cnmullins <cnmullins@mail.csuchico.edu>
2021-02-15 09:51:12 -0800

    MeleeEnemy ProtoType Demo

---
+++ Assets/Scripts/Enemy AI/BaseEnemy.cs
@@ -0,0 +1,78 @@
+﻿/*
+Author: Christian Mullins
+Date: 02/11/2021
+Summary: Parent script to all enemy AI.
+*/
+using UnityEngine;
+
+public class BaseEnemy : MonoBehaviour {
+    public int health;
+    public float speed;
+    public int damage;
+    public float attackFrequency;
+    [Tooltip("Denoted by red wire sphere.")]
+    public float rangeOfAttack;
+    [Tooltip("Denoted by yellow wire sphere.")]
+    public float aggroArea;
+    public bool isAggroed;
+
+    protected float attackTimer;
+    protected static Transform playerTrans;
+
+    protected virtual void Start() {
+        isAggroed = false;
+        attackTimer = 0f;
+        playerTrans = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    protected virtual void FixedUpdate() {}
+
+    /// <summary>
+    /// Inheritted function that will act differently depending on the enemy.
+    /// </summary>
+    protected virtual void Attack() {}
+
+    /// <summary>
+    /// Public variable that enables enemies to take damage from other scripts.
+    /// </summary>
+    /// <param name="damage">Damage value that the enemy will take.</param>
+    public void TakeDamage(int damageTaking) {
+        health -= damageTaking;
+        if (health < 0) Destroy(gameObject);
+    }
+
+    /// <summary>
+    /// Check if the player is in aggroArea and adjust isAggroed accordingly.
+    /// </summary>
+    /// <returns>Bool if enemy is aggroed.</returns>
+    protected bool IsPlayerInAggroRange() {
+        if (Vector3.Distance(transform.position, playerTrans.position) <= aggroArea) {
+            isAggroed = true;
+            return true;
+        }
+        isAggroed = false;
+        return false;
+    }
+
+    /// <summary>
+    /// Check if the player is in attack range.
+    /// </summary>
+    /// <returns>Bool if enemy is inside of attack range.</returns>
+    protected bool IsPlayerInAttackRange() {
+        return (Vector3.Distance(transform.position, playerTrans.position) 
+                <= rangeOfAttack);
+    }
+
+    /// <summary>
+    /// Draws spheres to give visual feedback on the range of attack and aggro
+    /// area of the enemy selected.
+    /// </summary>
+    private void OnDrawGizmosSelected() {
+        //draw agggro range
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, aggroArea);
+        //draw attack range
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, rangeOfAttack);
+    }
+}
---
+++ Assets/Scripts/Enemy AI/MeleeEnemy.cs
@@ -0,0 +1,67 @@
+﻿/*
+Author: Christian Mullins
+Date: 02/13/2021
+Summary: Enemy specializing in close combat, inheritted from base enemy class.
+*/
+using System.Collections;
+using UnityEngine;
+
+public class MeleeEnemy : BaseEnemy {
+    private float storeSpeed; // altering speed for charging values
+    private float storePlayerSpeed;
+
+    protected override void Start() {
+        isAggroed = false;
+        attackTimer = attackFrequency; // initialize an attack to occur
+        playerTrans = GameObject.FindGameObjectWithTag("Player").transform;
+        storePlayerSpeed = playerTrans.GetComponent<Character>().speed;
+        storeSpeed = speed;
+    }
+
+    protected override void FixedUpdate() {
+        // aggro and attack
+        if (!IsPlayerInAggroRange()) return;
+        if (IsPlayerInAttackRange() && attackTimer >= attackFrequency) { 
+            Attack();
+            attackTimer = 0f;
+        }
+
+        // manage attack timer
+        if (isAggroed) attackTimer += Time.deltaTime;
+
+        // movement
+        Vector3 movePos = playerTrans.position - transform.position;
+        movePos.y = 0f;
+        transform.position += movePos.normalized * speed * Time.deltaTime;
+        // adjust rotation to always look forward
+        transform.LookAt(movePos + transform.position, transform.up);
+    }
+
+    /// <summary>
+    /// Melee implementation of enemy attack by charging the player.
+    /// </summary>
+    protected override void Attack() {
+        // charge player
+        speed = storePlayerSpeed * 1.25f;
+        // cooldown
+        StartCoroutine("AttackCoolDown");
+    }
+
+    /// <summary>
+    /// Called inside Attack() to reset relavent values for new attack.
+    /// </summary>
+    private IEnumerator AttackCoolDown() {
+        yield return new WaitForSeconds(1f);
+        // step one
+        print("Backing up!");
+        speed = -speed;
+        attackTimer = 0f;
+        yield return new WaitForSeconds(0.5f);
+        // step two
+        speed = 0f;
+        yield return new WaitForSeconds(0.25f);
+        // leave coroutine
+        speed = storeSpeed;
+        yield return null;
+    }
+}
