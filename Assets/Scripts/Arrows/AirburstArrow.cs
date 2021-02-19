f7a310e3eae91ee92bc425b1377d0a696f667bdd
cnmullins <cnmullins@mail.csuchico.edu>
2021-02-19 15:10:53 -0800

    BombEnemy Demo is good to go.

--- Assets/Scripts/Arrows/AirburstArrow.cs
+++ Assets/Scripts/Arrows/AirburstArrow.cs
@@ -19,17 +19,33 @@ public class AirburstArrow : BaseArrow {
         Collider[] hits = Physics.OverlapSphere(transform.position, burstRadius);
         
         foreach (var h in hits) {
-            //explosion
-            Rigidbody hRB = h.attachedRigidbody;
-            if (hRB != null) {
-                hRB.AddExplosionForce(burstPower, transform.position, burstRadius, 0.75f, ForceMode.Impulse);
-            }
-            //calculate damage if this is an enemy
-            if (h.tag == "Enemy") {
-                int damage = (int)(burstRadius - Vector3.Distance(transform.position, h.transform.position));
-                damage *= 20;
-                damage = Mathf.Clamp(damage, 0, damage);
-                //h.GetComponent<BaseEnemy>().health -= damage;
+            if (h.gameObject != gameObject) {
+                print("hit: " + h.name);
+                // explosion
+                Rigidbody hRB = h.attachedRigidbody;
+                if (hRB != null) {
+
+                    hRB.AddExplosionForce(burstPower, transform.position, burstRadius, 0.75f, ForceMode.Impulse);
+                }
+                // calculate damage if this is an enemy
+                if (h.tag == "Enemy") {
+                    int damage = (int)(burstRadius - Vector3.Distance(transform.position, h.transform.position));
+                    damage *= 20;
+                    damage = Mathf.Clamp(damage, 0, damage);
+                    h.GetComponent<BaseEnemy>().health -= damage;
+                }
+                // handle player collision without rigidBody
+                if (h.tag == "Player") {
+                    var cc = h.GetComponent<CharacterController>();
+                    // apply force to CC velocity
+                    Vector3 forceDir =  h.transform.position - transform.position;
+                    float clampVal = Vector3.Distance(transform.position, h.transform.position);
+                    forceDir = Vector3.ClampMagnitude(forceDir, 0.25f);
+                    cc.enabled = false;
+                    h.transform.position += forceDir * burstPower;
+                    cc.enabled = true;
+                }
+                
             }
         }
     }
