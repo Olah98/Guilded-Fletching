54ed79ee2046134b3de7123ec25c334941107a45
Olah98 <aolah@mail.csuchico.edu>
2021-03-08 19:32:10 -0800

    Updated issues

--- Assets/Scripts/Temp/MainMenuButtons.cs
+++ Assets/Scripts/Temp/MainMenuButtons.cs
@@ -10,47 +10,51 @@ public class MainMenuButtons : MonoBehaviour
     private void Awake()
     {
         SaveManager.instance.Load();
-        if (SaveManager.instance.activeSave.sceneName!=null)
+        if (cont != null)
         {
-            cont.GetComponent<Button>().interactable = false;
+            if (SaveManager.instance.activeSave.sceneName != null)
+            {
+                cont.GetComponent<Button>().interactable = false;
+            }
         }
     }
 
-    public void LoadScene(string level)
-    {
-        SceneManager.LoadScene(level);
-    }
-
-    public void OpenLink(string url)
-    {
-        Application.OpenURL(url);
-    }
-
-    public void ExitGame()
-    {
-        Application.Quit();
-    }
-
-    public void LoadOptions() {
-        SceneManager.LoadScene("Options");
-    }
-
-    public void Continue()
-    {
-        SaveManager.instance.Load();
-        string sceneToLoad = SaveManager.instance.activeSave.sceneName;
-        if (sceneToLoad !=null &&
-            (sceneToLoad != "AlexOlahTest" ||
-            sceneToLoad != "Christian's Test Scene" ||
-            sceneToLoad != "Jon_Test" ||
-            sceneToLoad != "MilesGomezTest" ||
-            sceneToLoad != "TylerMunstockTest" ||
-            sceneToLoad != "Warren_Test"))
+        public void LoadScene(string level)
         {
-            SceneManager.LoadScene(SaveManager.instance.activeSave.sceneName);
+            SceneManager.LoadScene(level);
         }
-        
+
+        public void OpenLink(string url)
+        {
+            Application.OpenURL(url);
+        }
+
+        public void ExitGame()
+        {
+            Application.Quit();
+        }
+
+        public void LoadOptions()
+        {
+            SceneManager.LoadScene("Options");
+        }
+
+        public void Continue()
+        {
+            SaveManager.instance.Load();
+            string sceneToLoad = SaveManager.instance.activeSave.sceneName;
+            if (sceneToLoad != null &&
+                (sceneToLoad != "AlexOlahTest" ||
+                sceneToLoad != "Christian's Test Scene" ||
+                sceneToLoad != "Jon_Test" ||
+                sceneToLoad != "MilesGomezTest" ||
+                sceneToLoad != "TylerMunstockTest" ||
+                sceneToLoad != "Warren_Test"))
+            {
+                SceneManager.LoadScene(SaveManager.instance.activeSave.sceneName);
+            }
+
+        }
+
+
     }
-
-
-}
