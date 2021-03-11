77d25a8c5b06d3e7f7d065dc4ecc726f59b26f3e
Warren Rose II <TreeSquirrel84@gmail.com>
2021-03-11 12:04:26 -0800

    Added Color Changing Code

--- Assets/Scripts/Environmental/Door.cs
+++ Assets/Scripts/Environmental/Door.cs
@@ -13,8 +13,22 @@ public class Door : MonoBehaviour {
     public List<Switch> mySwitches;
     private Transform slideTo;
 
+    //By Warren
+    //Edit to change whether color or material is being affected.
+    //public Material m_isOn;
+    //public Material m_isOff;
+    private Color isOn = Color.green;
+    private Color isOff = Color.red;
+    private MeshRenderer rend;
+
+
     void Start() {
         slideTo = transform.GetChild(0);
+       
+        //By Warren
+        rend = GetComponent<MeshRenderer>();
+        UpdateColor(true);
+        IsAllSwitchesFlipped();
     }
 
     /// <summary>
@@ -23,9 +37,13 @@ public class Door : MonoBehaviour {
     /// <returns>True if all switches are flipped.</returns>
     public bool IsAllSwitchesFlipped() {
         foreach (var s in mySwitches) {
-            if (!s.isFlipped) 
+            if (!s.isFlipped)
+            {
+                UpdateColor(false);
                 return false;
+            }
         }
+        UpdateColor(true);
         return true;
     }
 
@@ -55,5 +73,28 @@ public class Door : MonoBehaviour {
             return true;
 
         return false;
+    }
+
+    /// <summary>
+    /// Function to call when changing color or material.
+    /// Edit to change whether color or material is being affected.
+    /// By Warren
+    /// </summary>
+    public void UpdateColor(bool isActive)
+    {
+        Color change;
+        //Material m_change;
+        if (isActive)
+        {
+            change = isOn;
+            //m_change = m_isOn;
+        }
+        else
+        {
+            change = isOff;
+            //m_change = m_isOff;
+        }
+        rend.material.SetColor("_Color", change);
+        //rend.material = m_change;
     }
 }
--- Assets/Scripts/Environmental/Switch.cs
+++ Assets/Scripts/Environmental/Switch.cs
@@ -10,9 +10,28 @@ public class Switch : MonoBehaviour {
     public Door myDoor; // can be a door or ladder
     public bool isFlipped;
 
+    //By Warren
+    //Edit to change whether color or material is being affected.
+    //public Material m_isOn;
+    //public Material m_isOff;
+    private Color isOn = Color.green;
+    private Color isOff = Color.red;
+    private MeshRenderer rend;
+
     void Start() {
+
+        //By Warren
+        rend = GetComponent<MeshRenderer>();
+        UpdateColor();
+
+
         // push this Switch into the Switch List for myDoor
         myDoor.mySwitches.Add(this);
+
+        if (!isFlipped)
+        {
+            myDoor.UpdateColor(false); //By Warren, updates color
+        }
     }
 
     /// <summary>
@@ -22,6 +41,7 @@ public class Switch : MonoBehaviour {
         if (isFlipped) return;
 
         isFlipped = true;
+        UpdateColor();//By Warren
         if (myDoor.IsAllSwitchesFlipped()) {
             StartCoroutine(myDoor.SlideOpen());
         }
@@ -35,5 +55,27 @@ public class Switch : MonoBehaviour {
         if (other.transform.tag == "Arrow") {
             HitSwitch();
         }
+    }
+
+    /// <summary>
+    /// Function to call when changing color or material.
+    /// Edit to change whether color or material is being affected.
+    /// By Warren
+    /// </summary>
+    private void UpdateColor()
+    {
+        Color change;
+        //Material m_change;
+        if (isFlipped)
+        {
+            change = isOn;
+            //m_change = m_isOn;
+        } else
+        {
+            change = isOff;
+            //m_change = m_isOff;
+        }
+        rend.material.SetColor("_Color", change);
+        //rend.material = m_change;
     }
 }
