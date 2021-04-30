01f29c0ab7b09cd54a045672ac6814591316a60e
Warren Rose II <TreeSquirrel84@gmail.com>
2021-04-29 22:59:40 -0700

    base script

---
+++ Assets/Scripts/UI/DisplayKeys.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayKeys : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public string ListKeys(string name)
+    {
+        switch (name)
+        {
+            case "Jump":
+                break;
+            case "Interact":
+                break;
+            case "Standard":
+                break;
+            case "Bramble":
+                break;
+            case "Warp":
+                break;
+            case "Airburst":
+                break;
+            case "Cancel":
+                break;
+            case "Movement":
+                break;
+            case "Look":
+                break;
+            case "Crouch":
+                break;
+            case "Fire":
+                break;
+            case "Zoom":
+                break;
+            default:
+                break;
+        }
+        return "";
+    }
+}
