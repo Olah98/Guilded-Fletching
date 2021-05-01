4acdc9b1f33e1505b18f97f75bf9ebc62ad8fc10
Warren Rose II <TreeSquirrel84@gmail.com>
2021-05-01 04:13:26 -0700

    First Pass of Key to Text

--- Assets/Scripts/UI/DisplayKeys.cs
+++ Assets/Scripts/UI/DisplayKeys.cs
@@ -1,9 +1,18 @@
-﻿using System.Collections;
+﻿/*
+ * 
+ * Referencing: DapperDino's UI Tutorial https://youtu.be/dUCcZrPhwSo
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DisplayKeys : MonoBehaviour
 {
+
+    private Controls _controls;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +25,71 @@ public class DisplayKeys : MonoBehaviour
 
     }
 
-    public string ListKeys(string name)
+    private void Awake()
+    {
+        _controls = new Controls();
+    }
+
+    private void OnEnable()
+    {
+        _controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _controls.Disable();
+    }
+
+    public InputAction ActionByName(string name)
+    {
+        InputAction result = null;
+        switch (name)
+        {
+            case "Jump":
+                result = _controls.Player.Jump;
+                break;
+            case "Interact":
+                result = _controls.Player.Interact;
+                break;
+            case "Standard":
+                result = _controls.Player.Standard;
+                break;
+            case "Bramble":
+                result = _controls.Player.Bramble;
+                break;
+            case "Warp":
+                result = _controls.Player.Warp;
+                break;
+            case "Airburst":
+                result = _controls.Player.Airburst;
+                break;
+            case "Cancel":
+                result = _controls.Player.Cancel;
+                break;
+            case "Movement":
+                result = _controls.Player.Movement;
+                break;
+            case "Look":
+                result = _controls.Player.Look;
+                break;
+            case "Crouch":
+                result = _controls.Player.Crouch;
+                break;
+            case "Fire":
+                result = _controls.Player.Fire;
+                break;
+            case "Zoom":
+                result = _controls.Player.Zoom;
+                break;
+            default:
+                break;
+        }
+        return result;
+    }
+
+
+
+    public string ArraySize(string name)
     {
         switch (name)
         {
@@ -48,5 +121,48 @@ public class DisplayKeys : MonoBehaviour
                 break;
         }
         return "";
+    }
+
+    public string KeyNumber(string name, int position)
+    {
+        switch (name)
+        {
+            case "Jump":
+                break;
+            case "Interact":
+                break;
+            case "Standard":
+                break;
+            case "Bramble":
+                break;
+            case "Warp":
+                break;
+            case "Airburst":
+                break;
+            case "Cancel":
+                break;
+            case "Movement":
+                break;
+            case "Look":
+                break;
+            case "Crouch":
+                break;
+            case "Fire":
+                break;
+            case "Zoom":
+                break;
+            default:
+                break;
+        }
+        return "" + position;
+    }
+
+    public string FirstKey(string name)
+    {
+        InputAction target = ActionByName(name);
+        string report = InputControlPath.ToHumanReadableString(
+            target.bindings[0].effectivePath,
+            InputControlPath.HumanReadableStringOptions.OmitDevice);
+        return report;
     }
 }
---
+++ Assets/Scripts/UI/HowToPlayUI.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HowToPlayUI : MonoBehaviour
+{
+    public TMP_Text controlsText;
+    public TMP_Text standText;
+    public TMP_Text brambText;
+    public TMP_Text warpText;
+    public TMP_Text airText;
+
+    private DisplayKeys _displayKeys;
+
+    void Awake()
+    {
+        _displayKeys = gameObject.AddComponent(typeof (DisplayKeys)) as DisplayKeys;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string _jumpKey = _displayKeys.FirstKey("Jump");
+        string _interactKey = _displayKeys.FirstKey("Interact");
+        string _standardKey = _displayKeys.FirstKey("Standard");
+        string _brambleKey = _displayKeys.FirstKey("Bramble");
+        string _warpKey = _displayKeys.FirstKey("Warp");
+        string _airburstKey = _displayKeys.FirstKey("Airburst");
+        string _cancelKey = _displayKeys.FirstKey("Cancel");
+        //string _movementKey = _displayKeys.FirstKey("Movement");
+        string _movementKey = "WASD";
+        //string _lookKey = _displayKeys.FirstKey("Look");
+        string _fireKey = _displayKeys.FirstKey("Fire");
+        string _crouchKey = _displayKeys.FirstKey("Crouch");
+        string _zoomKey = _displayKeys.FirstKey("Zoom");
+
+        controlsText.text = _movementKey + ": To Move\n" + 
+            _interactKey + ": Interact\n" +
+            _fireKey + ": Hold to Fire\n" + _zoomKey + ": Zoom\n" + 
+            _jumpKey + ": Jump\n" + _cancelKey + ": Pause Game\n" +
+           _standardKey + ", " + _brambleKey + ", "+ _warpKey + ", " +
+           _airburstKey + ": Equip Arrows\n" +
+             _crouchKey + ": Crouch";
+
+        standText.text = 
+            "Shoot targets with the normal arrow. It is on the " + _standardKey + " key.";
+        brambText.text = 
+            "Bind moving objects in place for seconds with the bramble arrow. " +
+            "Switch using the " + _brambleKey + " key. Try in level 2.";
+        warpText.text = 
+            "Teleport to certain targets by hitting them with the warp arrow. " +
+            "Switch using the " + _warpKey + " key. Needed to navigate level 5.";
+        airText.text = 
+            "Knock over light objects with the airburst arrow. " +
+            "Switch using the " + _airburstKey + " key.";
+    }
+}
