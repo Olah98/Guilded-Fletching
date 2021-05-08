6c16e5b447eb3ce77e21003bbca69d24ff5b5adf
mgomez74 <62723321+mgomez74@users.noreply.github.com>
2021-05-08 16:15:57 -0700

    Vines burn one another

---
+++ Assets/Burnable.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Burnable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public ParticleSystem Fire;
+    public GameObject Vine;
+    public bool burned;
+    LayerMask mask;
+
+    private void Start()
+    {
+        mask = LayerMask.GetMask("Burnable");
+    }
+
+    public void Burn()
+    {
+        burned = true;
+        Fire.Play();
+        Destroy(Vine, 1f);
+        Collider[] hits = new Collider[3];
+        // max number of collisions = hits.Length
+        
+        hits = Physics.OverlapBox(transform.position, transform.localScale, Quaternion.identity, mask);
+
+        foreach (Collider hit in hits)
+        {
+            if (hit.gameObject.tag == "Burnable" && !hit.gameObject.GetComponent<Burnable>().burned)
+            {
+                StartCoroutine(BurnDelay(hit.gameObject));
+            }
+            else
+            {
+                StartCoroutine(DestroyDelay());
+            }    
+        }
+        
+    }
+
+ 
+
+    IEnumerator BurnDelay(GameObject obj)
+    {
+        yield return new WaitForSeconds(.5f);
+        obj.GetComponent<Burnable>().Burn();
+        yield return new WaitForSeconds(.5f);
+        Fire.Stop();
+        Destroy(gameObject, 2f);
+
+    }
+
+    IEnumerator DestroyDelay()
+    {
+        yield return new WaitForSeconds(1f);
+        Fire.Stop();
+        Destroy(gameObject, 2f);
+    }
+
+}
