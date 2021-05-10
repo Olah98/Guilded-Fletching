c46bba2ccfd787f459627cf0f08fad0e76516af1
Olah98 <aolah@mail.csuchico.edu>
2021-05-10 16:51:44 -0700

    updates

---
+++ Assets/byby.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class byby : MonoBehaviour
+{
+    public float timetodie = 0;
+    private void Start()
+    {
+        Destroy(this.gameObject, timetodie);
+    }
+    private void Update()
+    {
+        if(Input.GetKey("escape"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+
+
+}
